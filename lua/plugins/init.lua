9ae5ba69475dec45d4fa90a3c26642d29c4a0775
Daniel-M <danielmejia55@gmail.com>
2023-12-13 16:40:55 -0500

    Adding copilot and custom plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,24 +4,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- {
-  --   "numToStr/Comment.nvim",
-  --   keys = {
-  --     { "gcc", mode = "n", desc = "Comment toggle current line" },
-  --     { "gc", mode = { "n", "o" }, desc = "Comment toggle linewise" },
-  --     { "gc", mode = "x", desc = "Comment toggle linewise (visual)" },
-  --     { "gbc", mode = "n", desc = "Comment toggle current block" },
-  --     { "gb", mode = { "n", "o" }, desc = "Comment toggle blockwise" },
-  --     { "gb", mode = "x", desc = "Comment toggle blockwise (visual)" },
-  --   },
-  --   init = function()
-  --     require("core.utils").load_mappings("comment")
-  --   end,
-  --   config = function(_, opts)
-  --     require("Comment").setup(opts)
-  --   end,
-  -- },
-
   {
     "rmehri01/onenord.nvim",
     config = {
@@ -48,7 +30,103 @@ return {
       --     match_paren = false,
       --   },
       --   custom_highlights = {}, -- Overwrite default highlight groups
-      --   custom_colors = {}, -- Overwrite default colors
+      -- custom_colors = {}, -- Overwrite default colors
     },
   },
+
+  -- Create annotations with one keybind, and jump your cursor in the inserted annotation
+  {
+    "danymat/neogen",
+    keys = {
+      {
+        "<leader>cc",
+        function()
+          require("neogen").generate({})
+        end,
+        desc = "Neogen Comment",
+      },
+    },
+    opts = { snippet_engine = "luasnip" },
+  },
+
+  -- Incremental rename
+  -- {
+  -- 	"smjonas/inc-rename.nvim",
+  -- 	cmd = "IncRename",
+  -- 	config = true,
+  -- },
+
+  -- Refactoring tool
+  {
+    "ThePrimeagen/refactoring.nvim",
+    keys = {
+      {
+        "<leader>r",
+        function()
+          require("refactoring").select_refactor()
+        end,
+        mode = "v",
+        noremap = true,
+        silent = true,
+        expr = false,
+      },
+    },
+    opts = {},
+  },
+
+  -- Go forward/backward with square brackets
+  {
+    "echasnovski/mini.bracketed",
+    event = "BufReadPost",
+    config = function()
+      local bracketed = require("mini.bracketed")
+      bracketed.setup({
+        file = { suffix = "" },
+        window = { suffix = "" },
+        quickfix = { suffix = "" },
+        yank = { suffix = "" },
+        treesitter = { suffix = "n" },
+      })
+    end,
+  },
+
+  -- Better increase/descrease
+  -- {
+  -- 	"monaqa/dial.nvim",
+  --    -- stylua: ignore
+  --    keys = {
+  --      { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
+  --      { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
+  --    },
+  -- 	config = function()
+  -- 		local augend = require("dial.augend")
+  -- 		require("dial.config").augends:register_group({
+  -- 			default = {
+  -- 				augend.integer.alias.decimal,
+  -- 				augend.integer.alias.hex,
+  -- 				augend.date.alias["%Y/%m/%d"],
+  -- 				augend.constant.alias.bool,
+  -- 				augend.semver.alias.semver,
+  -- 				augend.constant.new({ elements = { "let", "const" } }),
+  -- 			},
+  -- 		})
+  -- 	end,
+  -- },
+
+  {
+    "simrat39/symbols-outline.nvim",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    cmd = "SymbolsOutline",
+    opts = {
+      position = "right",
+    },
+  },
+
+  -- {
+  -- 	"nvim-cmp",
+  -- 	dependencies = { "hrsh7th/cmp-emoji" },
+  -- 	opts = function(_, opts)
+  -- 		table.insert(opts.sources, { name = "emoji" })
+  -- 	end,
+  -- },
 }
