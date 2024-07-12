df8832ad0d98f30f6603968c905bb5b9b638c9db
daniel-titan <daniel@titancharts.con>
2024-07-12 16:47:09 -0500

    New nvim configurations

---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,238 @@
+return {
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+	{ "cappyzawa/trim.nvim", opts = {} },
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		opts = {},
+		-- or                              , branch = "0.1.x",
+		-- dependencies = { "nvim-lua/plenary.nvim" }
+	},
+	{ "NvChad/nvim-colorizer.lua" },
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+	{
+		"ray-x/navigator.lua",
+		dependencies = {
+			{ "ray-x/guihua.lua", run = "cd lua/fzy && make" },
+			{ "neovim/nvim-lspconfig" },
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {},
+	},
+	--{ "sbdchd/neoformat", cmd = { "Neoformat" }},
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		opts = {
+			formatters_by_ft = {
+				css = { { "prettied", "prettier" } },
+				graphql = { { "prettied", "prettier" } },
+				html = { { "prettied", "prettier" } },
+				javascript = { { "prettied", "prettier" } },
+				javascriptreact = { { "prettied", "prettier" } },
+				json = { { "prettied", "prettier" } },
+				lua = { "stylua" },
+				markdown = { { "prettied", "prettier" } },
+				python = { "isort", "black" },
+				sql = { "sql-formatter" },
+				svelte = { { "prettied", "prettier" } },
+				typescript = { { "prettied", "prettier" } },
+				typescriptreact = { { "prettied", "prettier" } },
+				yaml = { "prettier" },
+			},
+			format_on_save = function(bufnr)
+				-- Disable autoformat on certain filetypes
+				local ignore_filetypes = { "sql", "java" }
+				if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+					return
+				end
+				-- Disable with a global or buffer-local variable
+				if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+					return
+				end
+				-- Disable autoformat for files in a certain path
+				local bufname = vim.api.nvim_buf_get_name(bufnr)
+				if bufname:match("/node_modules/") then
+					return
+				end
+				-- ...additional logic...
+				return { timeout_ms = 500, lsp_format = "fallback" }
+			end,
+		},
+		--config = function()
+		--  require('..config.formatting')
+		--end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		-- load cmp on InsertEnter
+		event = "InsertEnter",
+		-- these dependencies will only be loaded when cmp loads
+		-- dependencies are always lazy-loaded unless specified otherwise
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		},
+		opt = {
+			snippet = {
+				-- REQUIRED - you must specify a snippet engine
+				expand = function(args)
+					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+					-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+					-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+					-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+					-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
+				end,
+			},
+			window = {
+				-- completion = cmp.config.window.bordered(),
+				-- documentation = cmp.config.window.bordered(),
+			},
+			--mapping = cmp.mapping.preset.insert({
+			--	["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			--	["<C-f>"] = cmp.mapping.scroll_docs(4),
+			--	["<C-Space>"] = cmp.mapping.complete(),
+			--	["<C-e>"] = cmp.mapping.abort(),
+			--	["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			--}),
+			--sources = cmp.config.sources({
+			--	{ name = "nvim_lsp" },
+			--	{ name = "vsnip" }, -- For vsnip users.
+			--	-- { name = 'luasnip' }, -- For luasnip users.
+			--	-- { name = 'ultisnips' }, -- For ultisnips users.
+			--	-- { name = 'snippy' }, -- For snippy users.
+			--},
+			--{
+			--	{ name = "buffer" },
+			--}),
+		},
+	},
+	{ "neovim/nvim-lspconfig" },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = {
+			sort = {
+				sorter = "case_sensitive",
+			},
+			view = {
+				width = 30,
+			},
+			renderer = {
+				group_empty = true,
+			},
+			filters = {
+				dotfiles = true,
+			},
+		},
+	},
+
+	{ "nvim-tree/nvim-web-devicons", lazy = true },
+
+	{ "folke/todo-comments.nvim", opts = {} },
+
+	{
+		"dstein64/vim-startuptime",
+		-- lazy-load on a command
+		cmd = "StartupTime",
+		-- init is called during startup. Configuration for vim plugins typically should be set in an init function
+		init = function()
+			vim.g.startuptime_tries = 10
+		end,
+	},
+
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+	},
+
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = { "InsertEnter" },
+		opts = {},
+	},
+}
